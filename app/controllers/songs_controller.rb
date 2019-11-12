630c03038613fbe6415c080d290bd9b1970f6c42
vibraniumforge <gregm9999@gmail.com>
2019-11-12 14:22:34 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,23 +19,23 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song=Song.create({name: params[:song][:name]})
-    @song.artist=Artist.find_or_create_by(name: params[:artist][:name])
-    @song.genres << params[:genres].collect {|g| Genre.find(g)}
+    @song = Song.create({name: params[:song][:name]})
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genres << params[:genres].collect {|g| Genre.find(g)}.uniq
     @song.save
     flash[:message] = "Successfully created song."
     redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
-    @song=Song.find_by_slug(params[:slug])
+    @song = Song.find_by_slug(params[:slug])
     erb :'songs/edit'
   end
 
   patch '/songs/:slug' do
-    @song=Song.find_or_create_by({name: params[:name]})
-    @song.artist=Artist.find_or_create_by(name: params[:artist_name])
-    @song.genres << params[:genres].collect {|genre| Genre.find(genre)}
+    @song = Song.find_or_create_by({name: params[:name]})
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song.genres << params[:genres].collect {|genre| Genre.find(genre)}.uniq
     @song.save
     flash[:message] = "Successfully updated song."
     redirect to "songs/#{@song.slug}"
